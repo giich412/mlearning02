9c32c81de1d9cb7b831701ca27a92ce66e81cfb7
giich412 <161388298+giich412@users.noreply.github.com>
2024-09-06 06:48:24 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,15 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Machine Learning App 02')
 
 st.info('This App builds Machine Learning Model etc....')
 
 st.write('This App builds Machine Learning Model')
+
+st.set_page_config(page_title='CSV File Reader', layout='wide')
+st.header('Single File Upload')
+upload_file = st.file_uploader('Upload your CSV file')
+df = pd.read_csv(upload_file)
+st.dataframe(df, width=1800, height=1200)
+
