fdfa0f23a43a05d9b22daaa3e48dab9c1bf593e3
giich412 <161388298+giich412@users.noreply.github.com>
2024-09-06 06:19:05 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-st.title('Machine Learning App 01')
+st.title('Machine Learning App 02')
 
-st.info('This App builds Machine Learning Model')
+st.info('This App builds Machine Learning Model etc....')
 
 st.write('This App builds Machine Learning Model')
